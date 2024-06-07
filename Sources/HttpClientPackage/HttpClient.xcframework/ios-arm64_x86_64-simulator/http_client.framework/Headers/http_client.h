83cccf0202cb210cea6e04a033c5eb08994e4b1d
djadhav <Dhananjay.Jadhav@transformco.com>
2024-06-07 14:32:48 +0530

    updated xcframework

---
+++ Sources/HttpClientPackage/HttpClient.xcframework/ios-arm64_x86_64-simulator/http_client.framework/Headers/http_client.h
@@ -0,0 +1,18 @@
+//
+//  http_client.h
+//  http-client
+//
+//  Created by Jadhav, Dhananjay on 07/06/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for http_client.
+FOUNDATION_EXPORT double http_clientVersionNumber;
+
+//! Project version string for http_client.
+FOUNDATION_EXPORT const unsigned char http_clientVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <http_client/PublicHeader.h>
+
+
